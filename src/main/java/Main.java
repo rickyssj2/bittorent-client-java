8e606ae76794b885a42706dcfd9435ab000731f5
Aryan Pandey <aryanpandey@Riteshs-Mac-mini.local>
2024-10-30 00:13:00 -0700

    Decode Bencoded Strings and Integers using Java Generics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import com.google.gson.Gson;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+import com.dampcake.bencode.Bencode;
 
 public class Main {
   private static final Gson gson = new Gson();
@@ -9,35 +9,42 @@ public class Main {
     String command = args[0];
     if("decode".equals(command)) {
        String bencodedValue = args[1];
-       String decoded;
        try {
-         decoded = decodeBencode(bencodedValue);
-       } catch(RuntimeException e) {
-         System.out.println(e.getMessage());
-         return;
+        Object result = decode(bencodedValue);
+        if (result instanceof String) {
+          System.out.println(gson.toJson((String) result));
+        } else if (result instanceof Long) {
+          System.out.println(gson.toJson((Long) result));
+        }
+       } catch (Exception e) {
+        System.out.println("Invalid bencoded value: " + bencodedValue);
        }
-       System.out.println(gson.toJson(decoded));
-
     } else {
       System.out.println("Unknown command: " + command);
     }
 
   }
 
-  static String decodeBencode(String bencodedString) {
-    if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
-        }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    } else {
-      throw new RuntimeException("Only strings are supported at the moment");
+  public static <T> T decode(String bencodedString){
+    if (bencodedString == null || bencodedString.isEmpty()){
+      return null;
+    } else if (Character.isDigit(bencodedString.charAt(0))){
+      return (T) decodeString(bencodedString);      
+    } else if (bencodedString.startsWith("i")) {
+      return (T) decodeInteger(bencodedString);
     }
+    throw new IllegalArgumentException("Invalid bencoded format.");
+  }
+
+  public static Long decodeInteger(String bencodedString) {
+    int indexOfE = bencodedString.indexOf('e');
+    return Long.parseLong(bencodedString.substring(1, indexOfE));
+  }
+  
+  public static String decodeString(String bencodedString){
+    int firstColonIndex = bencodedString.indexOf(':');
+    int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
+    return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
   }
   
 }
